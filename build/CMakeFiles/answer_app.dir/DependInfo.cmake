dfe57967d592ab187b0a741850b0bd8f7523b488
luoyixun <luoyixun@zhejianglab.com>
2023-04-11 19:51:20 +0800

    modify curl_wrapper to link to curl not libcurl.so which can be cross platform

---
+++ build/CMakeFiles/answer_app.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/main.cpp" "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/build/CMakeFiles/answer_app.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../answer/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/build/answer/CMakeFiles/answer.dir/DependInfo.cmake"
+  "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/build/wolfram/CMakeFiles/wolfram.dir/DependInfo.cmake"
+  "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/build/curl_wrapper/CMakeFiles/curl_wrapper.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/cmake_install.cmake
@@ -0,0 +1,88 @@
+# Install script for directory: /Users/zarca/Desktop/Projects/git/modern-cmake-by-example
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/install_dir")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/objdump")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/build/bin/answer_app")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/answer_app" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/answer_app")
+    execute_process(COMMAND /usr/bin/install_name_tool
+      -delete_rpath "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/build/lib"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/answer_app")
+    execute_process(COMMAND /usr/bin/install_name_tool
+      -add_rpath "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/install_dir/lib"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/answer_app")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" -u -r "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/answer_app")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/doc/cmake/app" TYPE FILE FILES
+    "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/_COPYRIGHT"
+    "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/README.md"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE PROGRAM FILES "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/run_app.sh")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/doc/cmake/app" TYPE DIRECTORY FILES "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/doc/")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/build/answer/cmake_install.cmake")
+  include("/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/build/curl_wrapper/cmake_install.cmake")
+  include("/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/build/wolfram/cmake_install.cmake")
+
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/Users/zarca/Desktop/Projects/git/modern-cmake-by-example/build/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
